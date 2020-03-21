2da6133d27b192672871c071ad9c386d3e0b2a0c
Vitória Souza <vitoria.souza@cupula.local>
2020-03-21 18:20:42 -0300

    add link for delete todos

--- todos.js
+++ todos.js
@@ -10,7 +10,13 @@ function renderTodos() {
     var todoElement = document.createElement("li");
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
+    var linkText = document.createTextNode("Excluir");
+
+    linkElement.appendChild(linkText);
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
     listElement.appendChild(todoElement);
   }
 }
